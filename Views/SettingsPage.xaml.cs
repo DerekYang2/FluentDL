0dff9d3ebb27b25c98a220891a0897c2d6e41d4f
Derek Yang <115889767+DerekYang2@users.noreply.github.com>
2024-07-09 08:59:29 -0400

    Config path reading, segoe icons

--- Views/SettingsPage.xaml.cs
+++ Views/SettingsPage.xaml.cs
@@ -1,5 +1,6 @@
-﻿using FluentDL.ViewModels;
-
+﻿using System.Diagnostics;
+using FluentDL.Services;
+using FluentDL.ViewModels;
 using Microsoft.UI.Xaml.Controls;
 
 namespace FluentDL.Views;
@@ -16,5 +17,24 @@ public sealed partial class SettingsPage : Page
     {
         ViewModel = App.GetService<SettingsViewModel>();
         InitializeComponent();
+
+        // Run an async task to get the rip config path
+        new Task(() =>
+        {
+            var ripSubprocess = new RipSubprocess();
+            var ripConfigPath = ripSubprocess.RunCommandSync("rip config path");
+            ripSubprocess.Dispose();
+
+            // Find first index of ' and second index of ' in the string
+            var firstIndex = ripConfigPath.IndexOf('\'');
+            var secondIndex = ripConfigPath.IndexOf('\'', firstIndex + 1);
+            ripConfigPath = ripConfigPath.Substring(firstIndex + 1, secondIndex - firstIndex - 1);
+
+            configTextBlock.Text = ripConfigPath;
+
+            // Open the toml text file at ripConfigPath and save contents to string
+            var configFileStr = System.IO.File.ReadAllText(ripConfigPath);
+            Debug.WriteLine("CONFIG FILE:" + configFileStr);
+        }).Start();
     }
 }
