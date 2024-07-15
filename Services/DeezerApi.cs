a7297c0374e3979f78ecc544679d29b78e06d9c1
Derek Yang <115889767+DerekYang2@users.noreply.github.com>
2024-07-15 18:19:40 -0400

    Fixes to spotify loading and deezer api

--- Services/DeezerApi.cs
+++ Services/DeezerApi.cs
@@ -89,9 +89,68 @@ internal class DeezerApi
 
     public static async Task<JsonElement> FetchJsonElement(string req)
     {
-        var request = new RestRequest(req);
-        var response = await client.GetAsync(request);
-        return JsonDocument.Parse(response.Content).RootElement;
+        try
+        {
+            var request = new RestRequest(req);
+            var response = await client.GetAsync(request);
+            return JsonDocument.Parse(response.Content).RootElement;
+        }
+        catch (Exception e)
+        {
+            Debug.WriteLine("Failed: " + req);
+            req = req.Replace("(", "").Replace(")", ""); // Remove brackets, causes issues occasionally for some reason
+            var request = new RestRequest(req);
+            var response = await client.GetAsync(request);
+            return JsonDocument.Parse(response.Content).RootElement;
+        }
+    }
+
+    private static string PruneTitle(string title)
+    {
+        var titlePruned = title.ToLower().Trim();
+
+        // Remove (feat. X) from the title
+        var index = titlePruned.IndexOf("(feat.");
+        if (index != -1)
+        {
+            var closingIndex = titlePruned.IndexOf(")", index);
+            titlePruned = titlePruned.Remove(index, closingIndex - index + 1);
+        }
+
+        // Remove (ft. X) from the title
+        var index2 = titlePruned.IndexOf("(ft.");
+        if (index2 != -1)
+        {
+            var closingIndex = titlePruned.IndexOf(")", index2);
+            titlePruned = titlePruned.Remove(index2, closingIndex - index2 + 1);
+        }
+
+        // Remove punctuation that may cause inconsistency
+        titlePruned = titlePruned.Replace(" ", "").Replace("(", "").Replace(")", "").Replace("-", "").Replace(".", "").Replace("[", "").Replace("]", "");
+        return titlePruned;
+    }
+
+    private static string PruneTitleSearch(string title)
+    {
+        var titlePruned = title.ToLower().Trim();
+
+        // Remove (feat. X) from the title
+        var index = titlePruned.IndexOf("(feat.");
+        if (index != -1)
+        {
+            var closingIndex = titlePruned.IndexOf(")", index);
+            titlePruned = titlePruned.Remove(index, closingIndex - index + 1);
+        }
+
+        // Remove (ft. X) from the title
+        var index2 = titlePruned.IndexOf("(ft.");
+        if (index2 != -1)
+        {
+            var closingIndex = titlePruned.IndexOf(")", index2);
+            titlePruned = titlePruned.Remove(index2, closingIndex - index2 + 1);
+        }
+
+        return titlePruned;
     }
 
     public static async Task GeneralSearch(ObservableCollection<SongSearchObject> itemSource, string query)
@@ -117,61 +176,91 @@ internal class DeezerApi
 
     public static async Task<SongSearchObject> GeneralSearch(SongSearchObject song)
     {
-        var title = song.Title.ToLower();
+        var title = PruneTitleSearch(song.Title);
         var artists = song.Artists.Split(", ").ToList();
         var req = "search?q=" + artists[0] + " " + title; // Search for the first artist and title
-        Debug.WriteLine(req);
 
         var jsonObject = await FetchJsonElement(req);
+
+        SongSearchObject secondPriority = null, thirdPriority = null; // If no exact match, return the closest match
+
 
         foreach (var track in jsonObject.GetProperty("data").EnumerateArray()) // Check if at least one artist matches
         {
             var trackId = track.GetProperty("id").ToString();
             var songObj = await GetTrack(trackId); // Return the first result
 
-            bool exactTitleMatch = songObj.Title.ToLower().Equals(title);
 
             // Due to inconsistency of brackets and hyphens, delete them, all spaces, and compare
-            var titlePruned = title.Replace(" ", "").Replace("(", "").Replace(")", "").Replace("-", "").ToLower();
-            var songObjTitlePruned = songObj.Title.Replace(" ", "").Replace("(", "").Replace(")", "").Replace("-", "").ToLower();
+            var titlePruned = PruneTitle(title);
+            var songObjTitlePruned = PruneTitle(songObj.Title); // Remove periods due to inconsistency with Jr. and Jr
+
+            bool exactTitleMatch = titlePruned.Equals(songObjTitlePruned);
             bool substringMatch = titlePruned.Contains(songObjTitlePruned) || songObjTitlePruned.Contains(titlePruned);
 
 
             if (exactTitleMatch || substringMatch) // If the title matches
             {
                 List<string> songObjArtists = songObj.Artists.Split(", ").ToList();
+                bool allArtistsMatch = true, oneArtistMatch = false;
 
+                artists.Sort();
+                songObjArtists.Sort();
+
+                // Check if all artists match
+                if (artists.Count != songObjArtists.Count)
+                {
+                    allArtistsMatch = false;
+                }
+                else
+                {
+                    for (int i = 0; i < artists.Count; i++)
+                    {
+                        if (!songObjArtists[i].ToLower().Contains(artists[i].ToLower()) && !artists[i].ToLower().Contains(songObjArtists[i].ToLower())) // If neither works
+                        {
+                            allArtistsMatch = false;
+                            break;
+                        }
+                    }
+                }
+
+                // Check if at least one artist matches
                 foreach (var artist in artists)
                 {
                     foreach (var songObjArtist in songObjArtists)
                     {
-                        if (exactTitleMatch) // Be more lenient with artist name
+                        if (songObjArtist.ToLower().Contains(artist.ToLower()) || artist.ToLower().Contains(songObjArtist.ToLower())) // If artist names contain each other (sometimes inconsistency in artist name, The Black Eyed Peas vs Black Eyed Peas)
                         {
-                            if (songObjArtist.ToLower().Contains(artist.ToLower()) || artist.ToLower().Contains(songObjArtist.ToLower())) // If names contain each other
-                            {
-                                return songObj;
-                            }
-                        }
-                        else // Be more strict with artist name
-                        {
-                            if (songObjArtist.ToLower().Equals(artist.ToLower())) // If names are equal
-                            {
-                                return songObj;
-                            }
+                            oneArtistMatch = true; // If at least one artist matches
                         }
                     }
+                }
+
+                if (allArtistsMatch && exactTitleMatch)
+                {
+                    return songObj;
+                }
+
+                if (oneArtistMatch && exactTitleMatch) // If the title matches
+                {
+                    secondPriority = songObj; // Save for now, in case we find an exact match later
+                }
+
+                if (allArtistsMatch && substringMatch) // If the title is a substring match
+                {
+                    thirdPriority = songObj; // Save for now, in case we find an exact match later
                 }
             }
         }
 
-        return null; // If no results
+        return secondPriority ?? thirdPriority;
     }
 
     public static async Task<SongSearchObject> AdvancedSearch(string artistName, string trackName, string albumName)
     {
         // Trim
         artistName = artistName.Trim();
-        trackName = trackName.Trim();
+        trackName = PruneTitleSearch(trackName);
         albumName = albumName.Trim();
 
         if (artistName.Length == 0 && trackName.Length == 0 && albumName.Length == 0) // If no search query
@@ -181,11 +270,11 @@ internal class DeezerApi
 
         var req = "search?q=" + (artistName.Length > 0 ? "artist:%22" + artistName + "%22 " : "") + (trackName.Length > 0 ? "track:%22" + trackName + "%22 " : "") + (albumName.Length > 0 ? "album:%22" + albumName + "%22" : "") + "?strict=on"; // Strict search
         req = req.Replace(" ", "%20"); // Replace spaces with %20
-        req = req.Replace("(", "%20").Replace(")", "%20"); // Replace with spaces, brackets break advanced query for some reason
         req = req.Replace("&", "and");
         var jsonObject = await FetchJsonElement(req); // Create json object from the response
 
         SongSearchObject closeMatchObj = null;
+
 
         foreach (var track in jsonObject.GetProperty("data").EnumerateArray())
         {
@@ -193,18 +282,21 @@ internal class DeezerApi
 
             var songObj = await GetTrack(trackId); // Return the first result
 
-            if (songObj.Title.ToLower().Equals(trackName.ToLower())) // If the title matches
-            {
-                return songObj;
-            }
 
-            // Due to inconsistency of brackets and hyphens, delete them, all spaces, and compare
-            var titlePruned = trackName.Replace(" ", "").Replace("(", "").Replace(")", "").Replace("-", "").ToLower();
-            var songObjTitlePruned = songObj.Title.Replace(" ", "").Replace("(", "").Replace(")", "").Replace("-", "").ToLower();
+            var titlePruned = PruneTitle(trackName);
+            var songObjTitlePruned = PruneTitle(songObj.Title);
 
-            if (titlePruned.Equals(songObjTitlePruned)) // If the title is a substring match
+            if (titlePruned.Equals(songObjTitlePruned)) // If the title matches without punctuation
             {
-                closeMatchObj = songObj;
+                if (albumName.ToLower().Equals(songObj.AlbumName.ToLower())) // If the album name matches
+                {
+                    Debug.WriteLine(trackName);
+                    return songObj;
+                }
+                else
+                {
+                    closeMatchObj = songObj;
+                }
             }
         }
 
@@ -245,10 +337,18 @@ internal class DeezerApi
         var jsonObject = await FetchJsonElement("track/" + trackId);
 
         // Get the contributors of the track
+        HashSet<string> contributors = new HashSet<string>();
         var contribCsv = "";
+        Debug.WriteLine("New Track");
+
         foreach (var contribObject in jsonObject.GetProperty("contributors").EnumerateArray())
         {
-            contribCsv += contribObject.GetProperty("name").GetString() + ", ";
+            var name = contribObject.GetProperty("name").GetString();
+            if (!contributors.Contains(name) && !name.Contains(',')) // If the name is not already in the list and does not contain a comma
+            {
+                contribCsv += name + ", ";
+                contributors.Add(name);
+            }
         }
 
         contribCsv = contribCsv.Remove(contribCsv.Length - 2); // Remove the last comma and space
