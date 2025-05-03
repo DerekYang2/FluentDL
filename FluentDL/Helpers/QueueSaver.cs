533ec5647d4c591eafab94d43cf03ade94338641
Derek Yang <115889767+DerekYang2@users.noreply.github.com>
2025-05-03 14:15:37 -0400

    Fixed queue saver rewrite behavior

--- FluentDL/Helpers/QueueSaver.cs
+++ FluentDL/Helpers/QueueSaver.cs
@@ -10,12 +10,12 @@ namespace FluentDL.Helpers
 {
     internal class QueueSaver
     {
-        public static string fileName = Path.Combine(ApplicationData.Current.LocalCacheFolder.Path, "queue.json");
+        public static string fileName = Path.Combine(ApplicationData.Current.LocalFolder.Path, "queue.json");
         private static readonly object _lockObject = new object();
         private static StreamWriter? _writer = null;
 
         public static void Init() {
-            _writer = new StreamWriter(fileName, append: false) { AutoFlush = true };
+            _writer = new StreamWriter(fileName, append: false);
         }
         public static string GetPath() {
             return fileName;
@@ -24,7 +24,12 @@ namespace FluentDL.Helpers
         public static void SaveString(string str) {
             lock (_lockObject) {
                 if (_writer == null) return;
+        
+                // Set the position back to the beginning of the file
+                _writer.BaseStream.SetLength(0);
+                _writer.BaseStream.Position = 0;
                 _writer.Write(str);
+                _writer.Flush();
             }
         }
 
